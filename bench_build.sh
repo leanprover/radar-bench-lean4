6949f7cf854f1f0bf9f76511cb6f1a53e12c02dc
Joscha <joscha@plugh.de>
2025-11-20 13:43:21 +0100

    Rename stdlib benchmark to build

--- bench_build.sh
+++ bench_build.sh
@@ -6,18 +6,18 @@ REPO="$1" # absolute path to the repo to be benchmarked
 OUT="$2" # absolute path to the output jsonl file
 
 cd "$REPO"
-touch stdlib_upload_lakeprof_report
+touch build_upload_lakeprof_report
 
 if [ -d "tests/bench-radar" ]; then
   echo Using the bench-radar suite
-  tests/bench-radar/run_stdlib
-elif [ -d "tests/bench" ] && [ -f "tests/bench/run_stdlib" ]; then
+  tests/bench-radar/run_build
+elif [ -d "tests/bench" ] && [ -f "tests/bench/run_build" ]; then
   echo Using the bench suite
-  tests/bench/run_stdlib
+  tests/bench/run_build
 else
   echo Bringing my own copy of the bench-radar suite
   cp -r "$BENCH/bench-radar" tests/bench-radar
-  tests/bench-radar/run_stdlib
+  tests/bench-radar/run_build
 fi
 
 mv radar.jsonl "$OUT"
