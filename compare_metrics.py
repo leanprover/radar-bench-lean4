fec12ff93903e732c54a9736e5cd5ed720af4bf9
Joscha <joscha@plugh.de>
2026-02-19 17:44:48 +0100

    Rename old metrics to new bench suite names

---
+++ compare_metrics.py
@@ -0,0 +1,121 @@
+import argparse
+import json
+from pathlib import Path
+from typing import TypedDict
+
+
+def prompt(message: str, options: str = "Yn") -> str:
+    default: str | None = None
+    for c in options:
+        if c.isupper():
+            default = c.lower()
+            break
+
+    options = options.lower()
+    options_display = "/".join(c.upper() if c == default else c for c in options)
+
+    while True:
+        response = input(f"{message} [{options_display}]: ").strip().lower()
+        if not response and default:
+            return default
+        elif response in options.lower():
+            return response
+        else:
+            print(f"Please enter {options_display}.")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("first", type=Path, help="first measurements file")
+parser.add_argument("second", type=Path, help="second measurements file")
+args = parser.parse_args()
+
+first: Path = args.first
+second: Path = args.second
+
+renamings_file: Path = Path(__file__).parent / "renamings.json"
+renamings: dict[str, str | None] = json.loads(renamings_file.read_text())
+
+ignorings_file: Path = Path(__file__).parent / "ignorings.json"
+ignorings: set[str] = set(json.loads(ignorings_file.read_text()))
+
+
+def save_renamings():
+    renamings_file.write_text(json.dumps(renamings, indent=2, sort_keys=True) + "\n")
+
+
+def save_ignorings():
+    ignorings_file.write_text(
+        json.dumps(list(sorted(ignorings)), indent=2, sort_keys=True) + "\n"
+    )
+
+
+def rename(metric: str) -> str:
+    while (new_metric := renamings.get(metric)) is not None:
+        metric = new_metric
+    return metric
+
+
+class Measurement(TypedDict):
+    metric: str
+    first: float | None
+    second: float | None
+
+
+def load_measurements(file: Path) -> list[Measurement]:
+    measurements: list[Measurement] = []
+    with file.open() as f:
+        for line in f:
+            measurements.append(json.loads(line))
+    return measurements
+
+
+metrics_first = {rename(m["metric"]) for m in load_measurements(first)}
+metrics_second = {rename(m["metric"]) for m in load_measurements(second)}
+
+only_in_first = metrics_first - metrics_second - ignorings
+topics: dict[str, str] = {}
+for metric in sorted(only_in_first):
+    topic, *category = metric.rsplit("//", 1)
+    if topic in topics:
+        while topic in topics:
+            topic = topics[topic]
+        new_metric = "//".join([topic] + category)
+        renamings[metric] = new_metric
+        save_renamings()
+
+        print(f"{metric} :: {new_metric}")
+        continue
+
+    print()
+    print(f"{metric} :: ???")
+    answer = prompt("[r]ename, rename [t]opic, [i]gnore?", "rTi")
+
+    if answer == "r":
+        new_metric = input("New metric: ").strip()
+        print()
+
+        renamings[metric] = new_metric
+        save_renamings()
+
+        print(f"{metric} :: {new_metric}")
+        continue
+
+    if answer == "t":
+        new_topic = input("New topic: ").strip()
+        print()
+
+        topics[topic] = new_topic
+        new_metric = "//".join([new_topic] + category)
+        renamings[metric] = new_metric
+        save_renamings()
+
+        print(f"{metric} :: {new_metric}")
+        continue
+
+    assert answer == "i"
+    ignorings.add(metric)
+    save_ignorings()
+
+# Just to be sure
+save_renamings()
+save_ignorings()
