7ae5493ca22c3a896218773503a3dbc190cc6dbd
Joscha <joscha@plugh.de>
2026-07-10 16:37:42 +0200

    Simplify setup and specify lakeprof upload url

--- bench_build.sh
+++ bench_build.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
+export LAKEPROF_UPLOAD_URL="https://speed.lean-lang.org/lean4-out/"
+
 # Limit memory (all values are in KiB)
 mem_total="$(awk '/MemTotal/ { print $2 }' /proc/meminfo)"
 mem_limit="$((mem_total * 90 / 100))"
 ulimit -v "$mem_limit"
 
 cd "$RADAR_REPO"
-
-if [[ -f tests/bench_build.sh ]]; then
-  echo "Using tests/bench_build.sh"
-  timeout -s KILL 30m tests/bench_build.sh
-  "$RADAR_BENCH_REPO/rename_metrics.py" "$RADAR_OUT"
-  exit
-fi
-
-echo "Could not find benchmark suite."
-exit 1
+timeout -s KILL 30m tests/bench_build.sh
+"$RADAR_BENCH_REPO/rename_metrics.py" "$RADAR_OUT"
